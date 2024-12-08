88b55653ac41b9c6a26a0c978bf20973c0c00e0f
Sandeep Cheemakurthi <chsandeep2000@gmail.com>
2024-12-08 12:49:15 +0530

    NavBar Structure

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaReact } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { HiX } from "react-icons/hi";
 
 const data = [
     {
@@ -24,9 +27,37 @@ const data = [
 ]
 
 const NavBar = ()=>{
+    const [toggleIcon, setToggleIcon] = useState(false);
+
+    const handleToggleIcon = () => {
+        setToggleIcon(!toggleIcon)
+    }
+
     return (
         <div>
-            <label>NavBar</label>
+           <nav className="navbar">
+                <div className="navbar__container">
+                    <Link to={"/"} className="navbar__container__logo">
+                        <FaReact size={30}/>
+                    </Link>
+                </div>
+                <ul className="navbar__container__menu">
+                    {
+                        data.map((item, key) =>(
+                            <li key={key} className="navbar__container__menu__item">
+                                <Link className="navbar__container__menu__item__links" to={item.to}>
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className="nav-icon" onClick={handleToggleIcon}>
+                    {
+                        toggleIcon ? <HiX size={30}/>:<FaBars size={30}/>
+                    }
+                </div>
+           </nav>
         </div>
     )
 }
