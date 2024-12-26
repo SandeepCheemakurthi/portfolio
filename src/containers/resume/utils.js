9f62110645dc6232d7fe5003fb2107cd997663d3
Sandeep Cheemakurthi <chsandeep2000@gmail.com>
2024-12-26 22:52:23 +0530

    Resume Section

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,36 @@
+export const data = {
+    experience: [
+        {
+            title: "Associate",
+            subTitle: "Hyderabad",
+            description:"Conducted thorough module testing to identify and rectify issues, ensuring the reliability and stability of the applications. Guided juniors in implementing test-driven development (TDD) practices, improving code reliability and reducing bugs. Provided training on version control systems (e.g., Git), including branching, merging, and pull requests. Collaborated with the product owner, business analysts, and other developers using Agile methodologies,Scrum, and Git"
+        },
+        {
+            title: "Analyst",
+            subTitle: "Hyderabad",
+            description:""
+        },
+        {
+            title: "Senior Officer",
+            subTitle: "Hyderabad",
+            description:""
+        }
+    ],
+    education: [
+        {
+            title: "Bachelor's of Technology",
+            subTitle: "Gayatri Vidya Parishad",
+            description: "Computer Science and Engineering"
+        },
+        {
+            title: "Diploma",
+            subTitle: "Sanketika Vidya Parishad",
+            description: "Electrical and Electronics Engineering"
+        },
+        {
+            title: "Secondary",
+            subTitle: "NRI Indian Springs",
+            description: "State Board of Secondary Education"
+        }
+    ]
+}
