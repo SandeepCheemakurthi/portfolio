f7a9f14237ced5ba135d567ddfd0cb99b0157ad9
Sandeep Cheemakurthi <chsandeep2000@gmail.com>
2024-12-29 16:59:19 +0530

    Updated Resume data

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,19 +1,19 @@
 export const data = {
     experience: [
         {
-            title: "Associate",
+            title: "Associate at DBS Tech India, ",
             subTitle: "Hyderabad",
-            description:"Conducted thorough module testing to identify and rectify issues, ensuring the reliability and stability of the applications. Guided juniors in implementing test-driven development (TDD) practices, improving code reliability and reducing bugs. Provided training on version control systems (e.g., Git), including branching, merging, and pull requests. Collaborated with the product owner, business analysts, and other developers using Agile methodologies,Scrum, and Git"
+            description:"Conducted thorough module testing to identify and rectify issues, ensuring the reliability and stability of the applications. Guided juniors in implementing test-driven development (TDD) practices, improving code reliability and reducing bugs. Provided training on version control systems (e.g., Git), including branching, merging, and pull requests. Collaborated with the product owner, business analysts, and other developers using Agile methodologies, Scrum, and Git."
         },
         {
-            title: "Analyst",
+            title: "Analyst at DBS Tech India, ",
             subTitle: "Hyderabad",
-            description:""
+            description:"Created reusable UI components, accelerating development and ensuring consistency across multiple projects.Ensured efficient data processing, security, and scalability in application development.Utilized MariaDB and MySQL for database design, development, and optimization, enhancing application performance and data retrieval. Integrated RESTful APIs to enable seamless interaction between front-end and back-end components. Implemented a continuous integration/continuous deployment (CI/CD) pipeline to streamline project deployment."
         },
         {
-            title: "Senior Officer",
+            title: "Senior Officer at DBS Tech India, ",
             subTitle: "Hyderabad",
-            description:""
+            description:" Developed reusable UI components, ensuring consistency and reducing development time. Assisted in debugging and optimizing front-end performance for enhanced user experiences. Supported cross-functional teams in integrating solutions into the production environment."
         }
     ],
     education: [
